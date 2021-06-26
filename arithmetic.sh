bbce6333586c2994d93f19864b081f536be73c07
dksaini70 <dkreview70@gmail.com>
2021-06-26 21:04:03 +0530

    compute c+a/b

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 echo "arithmetic problem"
 echo "Enter the three number for arithmetic operation"
+add1=$(($a*$b+$c))
+echo $add1
 read a
 read b
 read c
@@ -9,3 +11,5 @@ add=$(($a+$b*$c))
 echo $add
 add1=$(($a*$b+$c))
 echo $add1
+add2=$(($c+$a/$b))
+echo $add2
